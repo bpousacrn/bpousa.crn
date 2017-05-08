3d34375ec854ec7118fe0b7862c00a947c304e39
bpousacrn <bpousa.crn@artsgrafiques.org>
2017-05-08 19:30:48 +0200

    Add files via upload

---
+++ HTML/javascript.js
@@ -0,0 +1,24 @@
+
+
+
+function drop() {
+    var x = document.getElementById("menu");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function sortir() {
+	alert
+}
+
+function myFunction(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
